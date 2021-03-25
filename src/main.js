01bc8e81ea359092329b8c481c81acfdddb4f25b
cindrmon1899 <feroxafarensis019@protonmail.com>
2021-03-25 12:56:07 +0800

    init necessary files

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
 
 Vue.config.productionTip = false;
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 
 new Vue({
   router,
---
+++ vue.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      "^/api": {
+        target: "http://localhost:8902",
+        changeOrigin: true,
+        logLevel: "debug",
+        pathRewrite: { "^/api": "/" },
+      },
+    },
+  },
+  chainWebpack: (config) => {
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
+      .options({
+        transformAssetUrls: {
+          video: ["src", "poster"],
+          source: "src",
+          img: "src",
+          image: "xlink:href",
+          "b-avatar": "src",
+          "b-img": "src",
+          "b-img-lazy": ["src", "blank-src"],
+          "b-card": "img-src",
+          "b-card-img": "src",
+          "b-card-img-lazy": ["src", "blank-src"],
+          "b-carousel-slide": "img-src",
+          "b-embed": "src",
+        },
+      });
+  },
+};
