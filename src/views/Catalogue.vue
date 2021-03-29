e711e17795373f24e9c360aa70e9f3853584fa11
cindrmon1899 <feroxafarensis019@protonmail.com>
2021-03-29 23:26:16 +0800

    revise catalogue grid

--- src/views/Catalogue.vue
+++ src/views/Catalogue.vue
@@ -1,34 +1,36 @@
 <template>
   <div class="catalogue container">
-    <h1 class="page-header">Our Watches</h1>
-    <b-card-group deck>
-      <b-card
-        v-for="product in watchCatalogue"
-        :key="product.id"
-        no-body
-        :img-src="require(`@/assets/images/catalogue/${product.productImage}`)"
-        :img-alt="image"
-        img-top
-        bg-variant="dark"
-        text-variant="white"
-        class="text-center"
-        style="max-width: 33.33%"
-      >
-        <b-card-body>
-          <b-card-title>
-            {{ product.productName }}
-          </b-card-title>
-          <b-card-sub-title class="mb-2">
-            XRP {{ product.productPrice }}
-          </b-card-sub-title>
-        </b-card-body>
-
-        <b-card-footer>
-          <b-button type="secondary"> Learn More </b-button>
-          <b-button type="primary"> Add To Cart </b-button>
-        </b-card-footer>
-      </b-card>
-    </b-card-group>
+    <b-row>
+      <h1 class="page-header">Our Watches</h1>
+    </b-row>
+    <b-row cols-sm="1" cols-md="3">
+      <b-col v-for="product in watchCatalogue" :key="product.id">
+        <b-card
+          no-body
+          :img-src="
+            require(`@/assets/images/catalogue/${product.productImage}`)
+          "
+          :img-alt="image"
+          img-top
+          bg-variant="dark"
+          text-variant="white"
+          class="text-center"
+        >
+          <b-card-body>
+            <b-card-title>
+              {{ product.productName }}
+            </b-card-title>
+            <b-card-sub-title class="mb-2">
+              XRP {{ product.productPrice }}
+            </b-card-sub-title>
+          </b-card-body>
+          <b-card-footer>
+            <b-button type="secondary">Learn More</b-button>
+            <b-button type="primary">Add to Cart</b-button>
+          </b-card-footer>
+        </b-card>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
